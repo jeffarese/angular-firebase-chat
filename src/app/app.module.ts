4413ac30632b13de48435bf02584a1ab1c430e21
Jeffarese <jeffarese@gmail.com>
2016-11-29 22:14:21 +0100

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBTKeSmJ-EZT8gFtdQxTdvcw3kDv1xK4pc',
+  authDomain: 'react-firebase-chat-74da6.firebaseapp.com',
+  databaseURL: 'https://react-firebase-chat-74da6.firebaseio.com',
+  storageBucket: 'react-firebase-chat-74da6.appspot.com',
+  messagingSenderId: '1005738784121'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   imports: [
     BrowserModule,
